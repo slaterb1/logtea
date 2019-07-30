47af85c27565e23aa0098856b8ff06f5b698c6eb
Benjamin Slater <ben@hackcapital.com>
2019-07-29 23:00:35 -0700

    updated parsers

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,11 +5,12 @@ use rettle::brewer::Brewery;
 use nom::{
     IResult,
     sequence::{delimited, preceded},
-    character::complete::char,
-    bytes::complete::{tag, is_not},
+    character::complete::{char, space1, alphanumeric0},
+    bytes::complete::{tag, is_not, take},
     combinator::opt,
 };
 
+#[derive(Debug)]
 struct LogTea {
     log_type: &'static str,
     datetime: &'static str,
@@ -17,35 +18,36 @@ struct LogTea {
     data: Option<&'static str>,
 }
 
-fn log_type(input: &str) -> IResult<&str, &str> {
+fn log_type(input: &'static str) -> IResult<&'static str, &'static str> {
     delimited(char('['), is_not("]"), char(']'))(input)
 }
 
 fn datetime(input: &str) -> IResult<&str, &str> {
-    delimited(char('-'), is_not(" "), char(' '))(input)
+    take(19u8)(input)
 }
 
 fn msg(input: &str) -> IResult<&str, &str> {
-    delimited(char('-'), is_not(" "), char(' '))(input)
+    alphanumeric0(input)
 }
 
-fn data(input: &str) -> IResult<&str, &str> {
-    delimited(char('-'), is_not(" "), char(' '))(input)
+fn data(input: &str) -> IResult<&str, Option<&str>> {
+    opt(delimited(char('-'), is_not(" "), char(' ')))(input)
 }
 
-fn parse_log(input: &str) -> IResult<&str, LogTea> {
+fn parse_log(input: &'static str) -> IResult<&str, LogTea> {
     let (input, log_type) = log_type(input)?;
-    let (input, _) = tag("- ")(input)?;
+    let (input, _) = tag(" - ")(input)?;
     let (input, datetime) = datetime(input)?;
+    let (input, _) = space1(input)?;
     let (input, msg) = msg(input)?;
-    let (input, data) = opt(data(input))(input)?;
-    (input, LogTea { log_type, datetime, msg, data })
+    let (input, data) = data(input)?;
+    Ok((input, LogTea { log_type, datetime, msg, data }))
 }
 
 fn main() {
     let test = "[INFO] - 2019-07-26T00:00:00 Server listening on port 8000";
     let test2 = "[ERROR] - 2019-07-26T00:00:00 Error on request! {status: 400, msg: Bad Request, data: []}";
-    let res = log_type(test);
-    let res2 = datetime(test);
-    println!("{:?}", res2);
+    let res = parse_log(test);
+    //let res2 = datetime(test);
+    println!("{:?}", res);
 }
